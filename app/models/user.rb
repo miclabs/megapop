5adaea9c8da7e421d13b3e6375cf4444c762b524
Liam <liam@miclabs.co>
2021-10-27 21:22:36 +0700

    Add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   enum role: { creator: 0, admin: 1, credit_manager: 2}
 
