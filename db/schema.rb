4cfed4057b4557518963ffebbcd2fa4f9495fda5
Liam <liam@miclabs.co>
2021-11-16 12:10:43 +0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_142635) do
+ActiveRecord::Schema.define(version: 2021_11_16_021058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,17 @@ ActiveRecord::Schema.define(version: 2021_11_12_142635) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "created_by_id"
     t.integer "updated_by_id"
+  end
+
+  create_table "rate_risk_adjustments", force: :cascade do |t|
+    t.integer "min_score"
+    t.integer "max_score"
+    t.float "risk_value"
+    t.integer "credit_tier"
+    t.boolean "eleigible_for_offer"
+    t.integer "rate_card_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "rates", force: :cascade do |t|
